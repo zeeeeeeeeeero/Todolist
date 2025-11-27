0d422d1f3aab6be3f123bf7ba729c85ba2cbe349
zeeeeeeeeeero <qq13931320105@163.com>
2025-11-28 02:22:07 +0800

    更新主类,但输入检查功能有问题

--- src/main/java/com/test/todolist/Main.java
+++ src/main/java/com/test/todolist/Main.java
@@ -1,13 +1,21 @@
 package com.test.todolist;
 
-import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-    private Scanner scanner = new Scanner(System.in);
-    private Stack<Runnable> menu = new Stack<>();
-
+    private static Stack<Runnable> menu = new Stack<>();
+    private static ScannerReader scannerReader = new ScannerReader();
     private static void mainMenu(){
-        System.out.println("欢迎使用ToDoList!\n输入功能代码:\n1.创建待办列表 2.");
+        System.out.println("欢迎使用ToDoList!");
+        System.out.println("输入需要操作的目录(没有将自动创建目录):");
+        int temp = scannerReader.getNextInt();
+        System.out.println(temp);
+    }
+
+    public static void main(String[] args) {
+        menu.push(Main::mainMenu);
+        while (!menu.isEmpty()){
+            menu.peek().run();
+        }
     }
 }
---
+++ src/main/java/com/test/todolist/ScannerReader.java
@@ -0,0 +1,20 @@
+package com.test.todolist;
+
+import java.util.Scanner;
+
+public class ScannerReader {
+    private final Scanner scanner = new Scanner(System.in);
+    protected int getNextInt(){
+        String temp = scanner.nextLine();
+        while (true) {
+            if (scanner.hasNextInt()) {
+                scanner.close();
+                return Integer.parseInt(temp);
+            } else {
+                System.out.println("无效输入!");
+                scanner.nextLine();
+            }
+        }
+    }
+
+}
